236f2230d4c9e24ca06f304066e483f8d296cd5b
sebastiancaicedo <sebastiancaicedo@uninorte.edu.co>
2018-03-28 21:24:39 -0500

    demo level 2-01, UI bugs

--- Assets/Scripts/LevelInfo.cs
+++ Assets/Scripts/LevelInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 [CreateAssetMenu(fileName = "LevelsInfo/New LevelInfo", menuName = "Geomits/LevelInfo", order = 1)]
 public class LevelInfo : ScriptableObject {
